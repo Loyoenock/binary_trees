541b7b6b9a4fdeb33689e67566e3878eaa18211d
Loyoenock <loyoenock@gmail.com>
2023-03-01 02:17:40 -0800

    0. New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,10 +4,11 @@
 
 /**
  * binary_tree_node - Create a binary node
- * @parent: Parent node
+ * @parent: node parent
  * @value: Value of the node
  * Return: the new node
  */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
