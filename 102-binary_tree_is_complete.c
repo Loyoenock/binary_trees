b495fdd0ec94ab67caf4eb326b6ea39db46e3974
Tusingura-rebecca <rtusingura@gmail.com>
2023-03-02 00:30:46 -0800

    complete binary tree

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,15 +1,15 @@
 #include "binary_trees.h"
+
 /**
-*new_node - Function that creates a new_node in a linked_list
-*@node: Type pointer of node to be created
-*
-*Return: the created node
-*/
+ * new_node - Function that creates a new_node in a linked_list
+ * @node: Type pointer of node to be created
+ * Return: the node created
+ */
 link_t *new_node(binary_tree_t *node)
 {
 	link_t *new;
 
-	new = malloc(sizeof(link_t));
+	new =  malloc(sizeof(link_t));
 	if (new == NULL)
 	{
 		return (NULL);
@@ -20,16 +20,16 @@ link_t *new_node(binary_tree_t *node)
 	return (new);
 }
 /**
-*free_q - Function that free the nodes at the linked list
-*@head: Node of the linked_list
-*/
+ * free_q - Function that free the nodes at the linked list
+ * @head: Node of the linked_list
+ */
 void free_q(link_t *head)
 {
 	link_t *temp_node;
 
 	while (head)
 	{
-		temp node = head->next;
+		temp_node = head->next;
 		free(head);
 		head = temp_node;
 	}
