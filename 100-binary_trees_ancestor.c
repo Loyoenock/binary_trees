fb5b92a1296c4e43a30af8dc6d4cee55c39c816f
Tusingura-rebecca <rtusingura@gmail.com>
2023-03-01 23:00:42 -0800

    ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+
+/**
+*binary_trees_ancestor - finds the lowest common ancestor of two nodes.
+*@first: Pointer to the first node
+*@second: Pointer to the second node
+*Return: if no common ancestors return NULL, else return common ancestor
+*/
+binary_tree_t *binary
+
